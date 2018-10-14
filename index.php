27b733aee9a0779105e09b957feda63bfe69b420
Andrii Pereverziev <y4t6official@gmail.com>
2018-10-14 22:33:09 +0300

    03_03_pdo_anonymous.php done

--- index.php
+++ index.php
@@ -11,4 +11,7 @@
 <a href='/02_07_exec_delete.php'>02_07_exec_delete.php</a><br>
 <a href='/02_08_pdo_error.php'>02_08_pdo_error.php</a><br>
 <a href='/02_08_pdo_exception.php'>02_08_pdo_exception.php</a><br>
+<a href='/02_09_pdo_quote.php'>02_09_pdo_quote.php</a><br>
+<a href='/03_02_pdo_named.php'>03_02_pdo_named.php</a><br>
+<a href='/03_03_pdo_anonymous.php'>03_03_pdo_anonymous.php</a><br>
 <a href='/'></a><br>
